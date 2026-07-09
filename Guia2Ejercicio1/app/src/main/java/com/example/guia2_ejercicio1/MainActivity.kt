cd25bf2b542bea2dca3d475314eb558fb10b753d
Katherine Renderos <kvrenderosr@gmail.com>
2026-07-09 10:08:27 -0600

    Parte 5 casi completa

--- Guia2Ejercicio1/app/src/main/java/com/example/guia2_ejercicio1/MainActivity.kt
+++ Guia2Ejercicio1/app/src/main/java/com/example/guia2_ejercicio1/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.guia2_ejercicio1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Variable
+    lateinit var btnSaludar: Button
+    lateinit var btnLimpiar: Button
+    lateinit var etNombre: EditText
+    lateinit var tvSaludo: TextView
+    lateinit var tvMensaje: TextView
+
+    // 2. Metodo del ciclo de vida del activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // 3. Establecer archivo Layout
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        
+        // 4. Referencias a elementos de la UI
+        btnSaludar = findViewById(R.id.btnSaludar)
+        btnLimpiar = findViewById(R.id.btnLimpiar)
+        etNombre = findViewById(R.id.etNombre)
+        tvSaludo = findViewById(R.id.tvSaludo)
+        tvSaludo = findViewById(R.id.tvMensaje)
+
+        tvSaludo.text = ""
+        tvSaludo.text = ""
+
+        // 5. Listeners de los botones
+
+
+    }
+    // Funcion auxiliar para mostrar mensajes emergente
+    private fun mostrarToast(
+        mensaje : String,
+        duracion: Int = Toast.LENGTH_SHORT
+    ){
+        Toast.makeText(
+            this,
+            mensaje,
+            duracion
+        ).show()
     }
 }
