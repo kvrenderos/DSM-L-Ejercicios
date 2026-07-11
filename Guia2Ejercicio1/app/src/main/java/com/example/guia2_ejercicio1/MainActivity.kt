da09e553e689b6780f6675acc87669765cd902ff
Katherine Renderos <kvrenderosr@gmail.com>
2026-07-10 20:16:31 -0600

    Ejercicio 1 (falta el analisis de resultado)

--- Guia2Ejercicio1/app/src/main/java/com/example/guia2_ejercicio1/MainActivity.kt
+++ Guia2Ejercicio1/app/src/main/java/com/example/guia2_ejercicio1/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.guia2_ejercicio1
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,43 +9,67 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    //Variable
-    lateinit var btnSaludar: Button
-    lateinit var btnLimpiar: Button
-    lateinit var etNombre: EditText
-    lateinit var tvSaludo: TextView
-    lateinit var tvMensaje: TextView
+    // Variables
+    private lateinit var btnSaludar: Button
+    private lateinit var btnLimpiar: Button
+    private lateinit var etNombre: EditText
+    private lateinit var tvSaludo: TextView
+    private lateinit var tvMensaje: TextView
 
-    // 2. Metodo del ciclo de vida del activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 3. Establecer archivo Layout
+        // Layout
         setContentView(R.layout.activity_main)
-        
-        // 4. Referencias a elementos de la UI
+
+        // Referencias
         btnSaludar = findViewById(R.id.btnSaludar)
         btnLimpiar = findViewById(R.id.btnLimpiar)
         etNombre = findViewById(R.id.etNombre)
         tvSaludo = findViewById(R.id.tvSaludo)
-        tvSaludo = findViewById(R.id.tvMensaje)
+        tvMensaje = findViewById(R.id.tvMensaje)
 
+        // Limpiar TextView al iniciar
         tvSaludo.text = ""
-        tvSaludo.text = ""
+        tvMensaje.text = ""
 
-        // 5. Listeners de los botones
+        // Botón Saludar
+        btnSaludar.setOnClickListener {
 
+            val nombre = etNombre.text.toString().trim()
 
+            if (nombre.isEmpty()) {
+                mostrarToast("Error, el campo nombre está vacío.")
+            } else {
+                val saludo = "Hola, $nombre"
+                val mensaje = "Bienvenido al laboratorio 2 de DSM441."
+
+                tvSaludo.text = saludo
+                tvMensaje.text = mensaje
+
+                mostrarToast("Saludo generado exitosamente.")
+            }
+        }
+
+        // Botón Limpiar
+        btnLimpiar.setOnClickListener {
+
+            etNombre.text.clear()
+
+            tvSaludo.text = ""
+            tvMensaje.text = ""
+
+            etNombre.requestFocus()
+
+            mostrarToast("Pantalla reiniciada.")
+        }
     }
-    // Funcion auxiliar para mostrar mensajes emergente
+
+    // Función auxiliar
     private fun mostrarToast(
-        mensaje : String,
+        mensaje: String,
         duracion: Int = Toast.LENGTH_SHORT
-    ){
-        Toast.makeText(
-            this,
-            mensaje,
-            duracion
-        ).show()
+    ) {
+        Toast.makeText(this, mensaje, duracion).show()
     }
 }
